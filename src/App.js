6553d3e98a4d2cb502cef22ecba4d2ec677d5bd4
Vladimir Mishenskiy <60189954+vovamishenskiy@users.noreply.github.com>
2023-04-21 16:52:47 +0400

    upd.
    added check for important notes, check for empty input field;
    added comments;
    made a little redesign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Note from './components/Note'
 
-function App() {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes) // creating notes entity
+  const [newNote, setNewNote] = useState('') // creating newNote entity to add new notes
+  const [showAll, setShowAll] = useState(true) // creating showAll entity to show either all notes or important ones
+  const [isImportant, setImportant] = useState(false) // creatint isImportant entity to check if note will be important
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: isImportant,
+      id: notes.length + 1,
+    }
+
+    // check if input field returned something - if no, then alert user, else add new note
+    if (!noteObject.content) return alert('You should enter something before saving note')
+
+    setNotes(notes.concat(noteObject)) // adding new note to note list
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => setNewNote(event.target.value)
+
+  // function that shows either all notes or important ones, if button return 'important' function returns only important notes and vice versa
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <div className='notes-form-container'>
+        <form onSubmit={addNote} className='notes-form'>
+          <input value={newNote} onChange={handleNoteChange} placeholder={'text'} />
+          <input checked={isImportant} onChange={() => setImportant(prev => !prev)} value={isImportant} type='checkbox' />
+          <button type='submit'>save</button>
+        </form>
+        <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
