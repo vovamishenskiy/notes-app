42a2284f6dba9e47409ea9ad23aa57f90c62aee9
Vladimir Mishenskiy <60189954+vovamishenskiy@users.noreply.github.com>
2023-04-28 16:28:47 +0400

    added first version of note settings popup + temp functions

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,31 @@
 import React, { useState } from 'react'
 
 const Note = ({ note }) => {
-    const [isShown, setIsShown] = useState(false)
+    const [isShownBtn, setIsShownBtn] = useState(false)
+    const [isShownPopup, setIsShownPopup] = useState(false)
+    const [isShownNote, setIsShownNote] = useState(true)
 
     return (
-        <li onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} className="notes-list-item">
-            {isShown && <button className='item-btn'>
-                <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z" /></svg>
-            </button>}
-            <span>{note.content}</span>
-        </li>
+        <div className='note-item-container'>
+            {isShownNote &&
+                <li onMouseEnter={() => setIsShownBtn(true)} onMouseLeave={() => setIsShownBtn(false)} className="notes-list-item">
+                    {isShownBtn &&
+                        <button className='item-btn' onClick={() => setIsShownPopup(prev => !prev)}>
+                            <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z" /></svg>
+                        </button>
+                    }
+                    <span>{note.content}</span>
+                </li>
+            }
+
+            {isShownPopup &&
+                <div className='item-popup' onMouseLeave={() => setIsShownPopup(false)}>
+                    <span>Note settings</span>
+                    <button onClick={() => setIsShownNote(false)}>delete</button>
+                    <button>move</button>
+                </div>
+            }
+        </div>
     )
 }
 
