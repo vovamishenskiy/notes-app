a2e32ff85624b7bae8e598d4af1f489fd1bf3400
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2023-05-08 17:53:04 +0400

    started implementing home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Notes from './components/Notes'
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp'
+import Welcome from './components/Welcome'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
-const App = ({notes}) => {
+const App = ({ notes }) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Navigate to='/signin' />} />
-        <Route path='/notes' element={<Notes notes={notes}/>} />
+        <Route path='/' element={<Navigate to='/welcome' />} />
+        <Route path='/notes' element={<Notes notes={notes} />} />
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/welcome' element={<Welcome />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+const Welcome = () => {
+    return (
+        <div>
+            <div className="header">
+                <h1>Hello</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
