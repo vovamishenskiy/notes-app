e68d592ff3d97213d06991c38053bde4630c60de
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2023-10-06 12:45:25 +0400

    started some ui work

--- notes-app/client/src/components/NoteInput.jsx
+++ notes-app/client/src/components/NoteInput.jsx
@@ -1,9 +1,9 @@
 const noteInput = ({ handleSubmit, handleInputChange, inputValue, updateNoteId }) => {
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="inputValue">input: </label>
-                <input type="text" id='inputValue' value={inputValue} onChange={handleInputChange} />
+        <div className="note-input-wrapper">
+            <form onSubmit={handleSubmit} className="note-input-form">
+                {/* <label htmlFor="inputValue">input: </label> */}
+                <input type="text" id='inputValue' value={inputValue} onChange={handleInputChange} placeholder="enter text" />
                 <button type='submit'>{updateNoteId ? 'update' : 'save'}</button>
             </form>
         </div>
--- notes-app/client/src/components/NoteList.jsx
+++ notes-app/client/src/components/NoteList.jsx
@@ -1,9 +1,9 @@
 const noteList = ({ notes, deleteNote, handleNoteEdit }) => {
     return (
-        <div>
-            <ul>
+        <div className="note-list-wrapper">
+            <ul className="note-list">
                 {notes.map((note) => (
-                    <div key={note.id}>
+                    <div key={note.id} className="note-list-item">
                         {note.content}
                         <button onClick={() => handleNoteEdit(note.id, note.content)}>edit note</button>
                         <button onClick={() => deleteNote(note._id)}>delete note</button>
